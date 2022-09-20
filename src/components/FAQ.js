5303fb0c535af4af39e476bf3fb5aaa62c96e5bd
Strelkova Olga <strelod@yandex.ru>
2022-09-20 19:44:00 +0300

    fix: btn & refactor: bem structure

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,36 +1,34 @@
-document.querySelectorAll('.frequent-questions__accordeon-item__trigger').forEach((item) => 
-item.addEventListener('click', () => { // мы выбрали все триггеры и для каждого из них повесили слушатель на клик
-    const parent = item.parentNode; //parentNode - это родительский узел
-    
-    
+document.querySelectorAll(".accordeon-item__trigger").forEach((item) =>
+	item.addEventListener("click", () => {
+		// мы выбрали все триггеры и для каждого из них повесили слушатель на клик
+		const parent = item.parentNode; //parentNode - это родительский узел
 
-    if (parent.classList.contains('frequent-questions__accordeon-item_active')) { //если родитель триггера имеет класс эктив
-        parent.classList.remove('frequent-questions__accordeon-item_active');// то по клику на триггер мы его удаляем
-      
-        
+		if (parent.classList.contains("accordeon-item_active")) {
+			console.log('я тут')
+			//если родитель триггера имеет класс эктив
+			parent.classList.remove("accordeon-item_active");
+			parent
+				.querySelector(".accordeon-item__button")
+				.classList.toggle("accordeon-item__button_active"); // то по клику на триггер мы его удаляем
+		} else {
+			//проверяем есть ли среди родителей всех элементов "accordeon-item_active"
+			document
+				.querySelectorAll(".accordeon-item") //то мы находим все аккордеон-айтемы
+				.forEach((elem) => {
+					if (elem.classList.contains("accordeon-item_active")) {
+						elem.classList.remove("accordeon-item_active");
+					}
+				}); //и для  каждого из них по клику удаляем клас эктив
+			parent.classList.toggle("accordeon-item_active");
 
-       /*document.querySelectorAll('.frequent-questions__accordeon-item__button')
-        .forEach((child) => child.classList.remove('frequent-questions__accordeon-item__button_active'));*/
-        
-      
-
-    } else { // если аккордеон-айтем не содержит класс актив
-        document
-        .querySelectorAll('.frequent-questions__accordeon-item') //то мы находим все аккордеон-айтемы
-        .forEach((child) => child.classList.remove('frequent-questions__accordeon-item_active')); //и для  каждого из них по клику удаляем клас эктив
-        parent.classList.toggle('frequent-questions__accordeon-item_active'); //а для родительского элеманта айтемов мы переключаем стиль на противоположный
-        console.log('!')
-      
-        //эта логика работает  для открытия второго айтема
-      
-
-      /* parent.querySelectorAll('.frequent-questions__accordeon-item__button')
-        .forEach((child) => child.classList.add('frequent-questions__accordeon-item__button_active'));
-        parent.classList.remove('frequent-questions__accordeon-item__button_active')*/
-        
-
-    
- }
-})
-)
-
+			document
+				.querySelectorAll(".accordeon-item__button")
+				.forEach((child) =>
+					child.classList.remove("accordeon-item__button_active")
+				);
+			parent
+				.querySelector(".accordeon-item__button")
+				.classList.add("accordeon-item__button_active");
+		}
+	})
+);
